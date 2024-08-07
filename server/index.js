397703700d4368f8be8ddfc7a2778bdf2a82d187
austeyen <austinchan09128@gmail.com>
2024-08-06 17:14:27 -0700

    login files

---
+++ server/index.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mysql = require("mysql2");
+const bodyParser = require("body-parser");
+
+// const db = mysql.createPool({
+//     host: "class3.hopekcc.org",
+//     user: "austin",
+//     password: "password",
+//     database: "testdb"
+// });
+
+const db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "testdb"
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post("/classroom/team-os/logindemo/api/register", (req, res) => {
+//app.post("/api/register", (req, res) => {
+
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+
+
+    const sqlInsert = "INSERT INTO userinfo (name, email, password) VALUES (?,?,?)";
+    db.query(sqlInsert, [name, email, password], (err, result) =>{
+        console.log(result);
+        console.log(err);
+    });
+}); 
+
+
+app.post("/classroom/team-os/logindemo/api/login", (req, res) => {
+//app.post("/api/login", (req, res) => {
+
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const sqlInsert = 'SELECT * FROM userinfo WHERE email = ? AND password = ?';
+    db.query(sqlInsert, [email, password], (err, result) => {
+        if(err){
+            console.log(err);
+        }
+        if(result.length === 0){ // need to clean up here
+            //res.send({message: "Invalid User!"});
+        }
+        //res.send(result);
+        res.send({message: "Valid User!"});
+
+    })
+}); 
+
+app.listen(3001, () => {
+    console.log("Running on Port 3001");
+});
